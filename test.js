607e638ff14bac5b064abba8e289cc314af19815
Andrew Johnson <ajzat25@gmail.com>
2020-02-25 19:38:54 -0500

    added test for nameless table

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 const tables = require('./index.js')
 
+console.log('named table: ')
+
 tables.log([
   ['Username', 'Status', 'Account #', 'Queries', 'cart', 'order status'],
   ['foo', 'online', '1', '54', 'eggs, milk, cheese', 'shipping'],
@@ -11,4 +13,18 @@ tables.log([
   header: '-',
   alignment: ['left', 'left', 'center', 'right'],
   name: 'Sample Table',
+})
+
+console.log('table without name: ')
+
+tables.log([
+  ['Username', 'Status', 'Account #', 'Queries', 'cart', 'order status'],
+  ['foo', 'online', '1', '54', 'eggs, milk, cheese', 'shipping'],
+  ['bar', 'offline', '3', '234', 'carrots', 'none'],
+  ['baz', 'loading', '2', '12824'],
+  ['eggs', 'offline', '3', '123', 'frying pan', 'out for delivery'],
+], {
+  walls:['┃ ', ' ┃ ', ' ┃'],
+  header: '-',
+  alignment: ['left', 'left', 'center', 'right'],
 })
